ec8178332767ed1be64933cd6179bc49f80299fc
AlecRandazzo <alec.randazzo@gmail.com>
2019-09-15 16:58:38 -0400

    fixed go test

--- timestamp_test.go
+++ timestamp_test.go
@@ -14,17 +14,23 @@ func TestTimeStamp_Parse(t *testing.T) {
 		name      string
 		timestamp TimeStamp
 		args      args
+		want      TimeStamp
 	}{
 		{
 			name: "Timestamp test",
 			args: args{
 				timestampBytes: timestampBytes,
 			},
-			timestamp: TimeStamp("2016-07-02T15:13:30Z"),
+			timestamp: TimeStamp(""),
+			want:      TimeStamp("2016-07-02T15:13:30Z"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt.timestamp.Parse(tt.args.timestampBytes)
+			if tt.timestamp != tt.want {
+				t.Errorf("TimeStamp.Parse() = %v, want %v", tt.timestamp, tt.want)
+			}
 		})
 	}
 }
