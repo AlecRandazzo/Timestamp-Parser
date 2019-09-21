44041a06b70acdec076a187b56bb638f96efe937
AlecRandazzo <alec.randazzo@gmail.com>
2019-09-20 21:31:54 -0400

    100% go test coverage

--- timestamp_test.go
+++ timestamp_test.go
@@ -14,23 +14,33 @@ func TestTimeStamp_Parse(t *testing.T) {
 		timestamp TimeStamp
 		args      args
 		want      string
+		wantErr   bool
 	}{
 		{
-			name: "Timestamp test",
+			name: "Timestamp test - valid timestamp",
 			args: args{
 				timestampBytes: []byte{234, 36, 205, 74, 116, 212, 209, 1},
 			},
 			timestamp: TimeStamp{},
 			want:      "2016-07-02T15:13:30Z",
+			wantErr:   false,
+		},
+		{
+			name: "Timestamp test - null timestamp",
+			args: args{
+				timestampBytes: []byte{},
+			},
+			timestamp: TimeStamp{},
+			want:      "",
+			wantErr:   true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.timestamp.Parse(tt.args.timestampBytes)
+			err := tt.timestamp.Parse(tt.args.timestampBytes)
 			ts := time.Time(tt.timestamp).Format("2006-01-02T15:04:05Z")
-			if ts != tt.want {
+			if ts != tt.want && (err != nil) != tt.wantErr {
 				t.Errorf("TimeStamp.Parse() = %v, want = %v", ts, tt.want)
-
 			}
 		})
 	}
